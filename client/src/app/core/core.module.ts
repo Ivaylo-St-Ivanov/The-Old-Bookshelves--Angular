4d2adbc4832601fc79865a6fb23341b9314a980a
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-07-05 19:08:31 +0300

    Add footer.

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-
-
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent,
-    HomeComponent
-  ]
+    declarations: [
+        HeaderComponent,
+        HomeComponent,
+        FooterComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        HeaderComponent,
+        HomeComponent,
+        FooterComponent
+    ]
 })
 export class CoreModule { }
