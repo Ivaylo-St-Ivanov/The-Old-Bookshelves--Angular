b148b345022ab7a572b18a326f3c9673a4877a54
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-07-09 15:45:56 +0300

    Add login page and fixes from register CSS file to global CSS file.

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -3,19 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
 import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     RouterModule
-  ],
-  exports: [
-    RegisterComponent
   ]
 })
 export class UserModule { }
