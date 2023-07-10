e213e29c34b0d93bfa3813cd7d05b83bf7df7ec0
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-07-10 15:14:34 +0300

    Add Http Client module and add Book interface.

---
+++ client/src/app/types/Book.ts
@@ -0,0 +1,9 @@
+export interface Book {
+    bookName: string;
+    author: string;
+    imageUrl: string;
+    cover: string;
+    coverPrice: number;
+    price: number;
+    description: string;
+}
