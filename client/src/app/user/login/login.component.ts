9dc460761ce5ebbcdccd43dd3372da9b964f4a3c
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-07-12 22:45:25 +0300

    Get data from login form, add user service and set fake user in local storage.

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/types/User';
+import { UserService } from '../user.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    constructor(private userService: UserService) { }
 
+    submitHandler(form: NgForm): void {
+        const value: User = form.value;
+
+        this.userService.login();
+        
+        form.setValue({ email: '', password: '' });
+    }
 }
--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -12,7 +13,8 @@ import { LoginComponent } from './login/login.component';
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ]
 })
 export class UserModule { }
