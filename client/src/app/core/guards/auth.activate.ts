c248825e31fd5ebbc86eff425092cf8257b3d511
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-07-13 21:19:10 +0300

    Update router guard with redirect to login page.

--- client/src/app/core/guards/auth.activate.ts
+++ client/src/app/core/guards/auth.activate.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
+
 import { UserService } from "src/app/user/user.service";
 
 @Injectable({ providedIn: 'root' })
 export class AuthActivate implements CanActivate {
-    constructor(private userService: UserService) {}
+    constructor(private userService: UserService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return (this.checkIsLogged() || this.router.createUrlTree(['/users/login']));
+    }
+
+    checkIsLogged(): boolean {
         return this.userService.isLogged;
     }
 }
