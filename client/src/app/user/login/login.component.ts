7c290fbd5b6992699b21eb38e709675e6f474455
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-07-13 12:50:28 +0300

    Update header and render it dynamically + implement logout.

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from 'src/app/types/User';
 import { UserService } from '../user.service';
 
@@ -9,12 +10,13 @@ import { UserService } from '../user.service';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-    constructor(private userService: UserService) { }
+    constructor(private userService: UserService, private router: Router) { }
 
     submitHandler(form: NgForm): void {
         const value: User = form.value;
 
         this.userService.login();
+        this.router.navigate(['/books/used-books']);
         
         form.setValue({ email: '', password: '' });
     }
