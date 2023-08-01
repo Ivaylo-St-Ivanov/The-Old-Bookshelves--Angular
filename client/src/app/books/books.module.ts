9c8def90927266cbc9e92479b0ea0f8b1c312e95
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-08-01 15:21:43 +0300

    Add error handling to Edit-book page.

--- client/src/app/books/books.module.ts
+++ client/src/app/books/books.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UsedBooksComponent } from './used-books/used-books.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { SellBookComponent } from './sell-book/sell-book.component';
 import { GuardPageComponent } from './guard-page/guard-page.component';
-import { FormsModule } from '@angular/forms';
 import { EditBookComponent } from './edit-book/edit-book.component';
 
 @NgModule({
@@ -20,7 +20,8 @@ import { EditBookComponent } from './edit-book/edit-book.component';
     imports: [
         CommonModule,
         RouterModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
     exports: [
         UsedBooksComponent,
--- client/src/app/books/edit-book/edit-book.component.ts
+++ client/src/app/books/edit-book/edit-book.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-edit-book',
-  templateUrl: './edit-book.component.html',
-  styleUrls: ['./edit-book.component.css']
+    selector: 'app-edit-book',
+    templateUrl: './edit-book.component.html',
+    styleUrls: ['./edit-book.component.css']
 })
 export class EditBookComponent {
+    constructor(private fb: FormBuilder) {}
 
+    form = this.fb.group({
+        bookName: ['', [Validators.required]],
+        imageUrl: ['', [Validators.required]],
+        author: ['', [Validators.required]],
+        cover:  [''],
+        coverPrice: ['', [Validators.required, Validators.min(0)]],
+        price: ['', [Validators.required, Validators.min(0)]],
+        description: ['']
+    });
+
+    onEditBookSubmit(): void {
+        if (this.form.invalid) {
+            return;
+        }
+    }
 }
