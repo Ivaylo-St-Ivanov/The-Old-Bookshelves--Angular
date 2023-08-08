c01dc505649a21c947c980da37a44abfa8e8e560
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-08-08 20:25:27 +0300

    Render dynamically five random books on the bottom of the Home page.

--- client/src/app/core/home/home.component.ts
+++ client/src/app/core/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Book } from 'src/app/types/Book';
+
+import { BooksService } from 'src/app/books/books.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    books: Book[] = [];
 
+    constructor(private bookService: BooksService) {}
+
+    ngOnInit(): void {
+        this.getFiveBooksRandom();
+    }
+
+    getFiveBooksRandom() {
+        this.bookService.getAllUsedBooks().subscribe((result) => {
+            const all = result.results;
+
+            const five = [];
+            const previous: number[] = [];
+
+            while (five.length < 5) {
+                const random = Math.floor(Math.random() * all.length);
+
+                if (previous.includes(random)) {
+                    continue;
+                }
+                five.push(all[random]);
+                previous.push(random);
+            }
+
+            this.books = five;
+        });
+    }
 }
